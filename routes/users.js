3f584ef5861e3aeed92a4dce4a1c35f8b6dd14ca
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-15 15:54:33 -0600

    required query file to routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const query = require('../db/query.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
