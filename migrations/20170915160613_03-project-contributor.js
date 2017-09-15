12c0a0901ebfbe4e668f969b299408659e758513
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-15 16:08:05 -0600

    created project-contributor table migration

---
+++ migrations/20170915160613_03-project-contributor.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('project_contributor', (table)=> {
+    table.increments();
+    table.float('amount', 8, 2).notNullable();
+    table.integer('project_id').references('account.id').onDelete('CASCADE');
+    table.integer('account_id').references('account.id').onDelete('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('project_contributor');
+};
