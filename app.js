95584e935e80477c6c8f95586ce081faa19da420
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-15 15:16:54 -0600

    added projects route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const projects = require('./routes/projects');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
