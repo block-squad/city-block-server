34c73d8b7253d478ec419911e522e2437a987ba7
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-17 12:45:17 -0600

    created all queries

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const accounts = require('./routes/accounts');
 const projects = require('./routes/projects');
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/accounts', accounts);
 app.use('/projects', projects);
 
 // catch 404 and forward to error handler
--- db/query.js
+++ db/query.js
@@ -13,15 +13,52 @@ module.exports = {
     return knex('project').where('id', id);
   },
   getAllContributionsForOneProject: (projectId)=>{
-    return
-      knex('project_contributor')
+    return knex('project_contributor')
       .join('project', 'project.id', '=', 'project_contributor.project_id')
-      .where('project_id', projectId)
+      .join('account', 'account.id', '=', 'project_contributor.account.id')
+      .where('project.id', projectId)
   },
   getAllContributionsForOneAccount: (accountId)=>{
-    return
-      knex('project_contributor')
-      .where('account_id', accountId);
+    return knex('project_contributor')
+      .join('project', 'project.id', '=', 'project_contributor.project_id')
+      .join('account', 'account.id', '=', 'project_contributor.account.id')
+      .where('account.id', accountId);
   },
-
+  postToAccount: (account)=>{
+    return knex('account')
+      .insert(account)
+      .returning('*')
+  },
+  postToProject: (project)=>{
+    return knex('project')
+      .insert(project)
+      .returning('*')
+  },
+  postContribution: (contribution)=>{
+    return knex('project_contributor')
+      .insert(contribution)
+      .returning('*')
+  },
+  updateAccount: (account, id)=>{
+    return knex('account')
+      .where('id', id)
+      .update(account)
+      .returning('*')
+  },
+  updateProject: (project, id)=>{
+    return knex('project')
+      .where('id', id)
+      .update(project)
+      .returning('*')
+  },
+  deleteAccount: (id) => {
+    return knex('account')
+      .where('id', id)
+      .del()
+  },
+  deleteProject: (id) => {
+    return knex('project')
+      .where('id', id)
+      .del()
+  }
 }
